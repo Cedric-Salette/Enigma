0166f87cf3bcf16ce180bf58a430637bae5ef91b
Cedric-Salette <c.salette@yahoo.com>
2020-06-10 14:22:30 +0200

    creating repository

---
+++ enigma.py
@@ -0,0 +1,8 @@
+print("hello World")
+
+
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+# Description des 5 rotors.
+rotor_1 = ['E','K','M','F','L','G','D','Q','V','Z','N','T','O','W','Y','H','X','U','S','P','A','I','B','R','C','J']
+rotor_2 = ['A','J','D','K','S','I','R','U','X','B','L','H','W','T','M','C','Q','G','Z','N','P','Y','F','V','O','E']
